2012112b6c8c59d0cac0c1696c099d959eec031e
joebalanoff <64605956+joebalanoff@users.noreply.github.com>
2022-02-09 11:11:00 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ const server = require('./server/app');
 const bot = require('./discord/discord');
 
 //bot.startBot().catch(err => console.error(err));
-server.startServer().catch(err => console.error(err));
+server.startServer(process.env.PORT).catch(err => console.error(err));
